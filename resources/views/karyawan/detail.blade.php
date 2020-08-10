d3b9f88fd86a38f233345a496b060f5d13ff14f4
Michaellim2000 <michael18001@mail.unpad.ac.id>
2020-08-10 21:49:40 +0700

    Kuis 3 Oh Final

---
+++ resources/views/karyawan/detail.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/master')
+@section('content')
+    <ul>
+        <li>Nama Proyek : {{ $proyek->nama_proyek }}</li>
+        <li>Jabaran : {{ $karyawan->jabatan }}</li>
+    </ul>
+@endsection
